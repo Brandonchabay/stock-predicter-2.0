998508da0d98c4276b62dc45bb3d6fc2626a1dbf
Brandon Chabay <chabayb@gmail.com>
2024-10-13 02:25:33 -0400

    buttons

--- stock-predicter-ui/src/app/Components/Infosection.js
+++ stock-predicter-ui/src/app/Components/Infosection.js
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import TextGeneratorEffect from './TextGeneratorEffect';
+import Link from "next/link";
+import TextGeneratorEffect from "./TextGeneratorEffect";
 
 export default function InfoSection() {
   return (
@@ -9,11 +10,11 @@ export default function InfoSection() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Already have an account?{'    '}
-              <a href="#" className="font-semibold text-indigo-600">
+              Already have an account?{"    "}
+              <Link href="/login" className="font-semibold text-indigo-600">
                 <span aria-hidden="true" className="absolute inset-0" />
                 Login here <span aria-hidden="true">&rarr;</span>
-              </a>
+              </Link>
             </div>
           </div>
           <div className="text-center">
@@ -26,12 +27,12 @@ export default function InfoSection() {
               speed={40}
             />
             <div className="mt-10 flex items-center justify-center align items: center;">
-              <a
-                href="#"
+              <Link
+                href="signup"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign Up
-              </a>
+              </Link>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
@@ -46,7 +47,7 @@ export default function InfoSection() {
           <div
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           />
