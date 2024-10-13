4a0457e5f138e77bbbc8175667fed20a14691af0
Krish Patel <krishpatel@Krishs-MacBook-Air-2.local>
2024-10-13 01:06:41 -0400

    fix nsv bar bug

--- stock-predicter-ui/src/app/Components/Infosection.js
+++ stock-predicter-ui/src/app/Components/Infosection.js
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import TextGeneratorEffect from "./TextGeneratorEffect";
+import TextGeneratorEffect from './TextGeneratorEffect';
 
 export default function InfoSection() {
   return (
-    <div className="bg-white -mt-44">
+    <div className="bg-white">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Already have an account?{"    "}
+              Already have an account?{'    '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span aria-hidden="true" className="absolute inset-0" />
                 Login here <span aria-hidden="true">&rarr;</span>
@@ -46,7 +46,7 @@ export default function InfoSection() {
           <div
             style={{
               clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           />
