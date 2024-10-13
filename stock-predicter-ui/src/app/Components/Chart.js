42b04efcf17258c392332bb0d246d027407bf419
merwin aamir <merwinaa@amazon.com>
2024-10-13 04:43:13 -0400

    add dashboard with stocks

---
+++ stock-predicter-ui/src/app/Components/Chart.js
@@ -0,0 +1,68 @@
+"use client";
+import { useState } from "react";
+import { AgFinancialCharts } from "ag-charts-react";
+import "ag-charts-enterprise";
+
+export default function Chart() {
+  const mockData = [
+    {
+      date: new Date("2024-10-01"),
+      open: 150,
+      high: 155,
+      low: 148,
+      close: 152,
+      volume: 3000,
+    },
+    {
+      date: new Date("2024-10-02"),
+      open: 152,
+      high: 160,
+      low: 151,
+      close: 157,
+      volume: 2800,
+    },
+    {
+      date: new Date("2024-10-03"),
+      open: 157,
+      high: 162,
+      low: 156,
+      close: 161,
+      volume: 3200,
+    },
+    {
+      date: new Date("2024-10-04"),
+      open: 161,
+      high: 165,
+      low: 160,
+      close: 163,
+      volume: 3500,
+    },
+    {
+      date: new Date("2024-10-05"),
+      open: 163,
+      high: 168,
+      low: 162,
+      close: 167,
+      volume: 3100,
+    },
+  ];
+  const [options] = useState({
+    data: mockData,
+    dateKey: "date",
+    openKey: "open",
+    highKey: "high",
+    lowKey: "low",
+    closeKey: "close",
+    volumeKey: "volume",
+    chartType: "candlestick",
+    navigator: true,
+    toolbar: true,
+    rangeButtons: true,
+    volume: true,
+    statusBar: true,
+    zoom: true,
+    barWidth: 1,
+  });
+
+  return <AgFinancialCharts options={options} />;
+}
---
+++ stock-predicter-ui/src/app/Components/StocksList.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+// Sample stock data
+const stocks = [
+  {
+    ticker: "AAPL",
+    price: "$120.00",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKbVja036CLC6tlqDn6YaAf163E4CiKyADvg&s",
+  },
+  {
+    ticker: "MSFT",
+    price: "$250.00",
+    imageUrl: "https://g.foolcdn.com/art/companylogos/square/msft.png",
+  },
+  {
+    ticker: "GOOG",
+    price: "$800.00",
+    imageUrl:
+      "https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw",
+  },
+  {
+    ticker: "AMZN",
+    price: "$820.21",
+    imageUrl:
+      "https://m.media-amazon.com/images/G/01/gc/designs/livepreview/amazon_dkblue_noto_email_v2016_us-main._CB468775337_.png",
+  },
+];
+
+export default function StockList({ onStockClick }) {
+  return (
+    <ul
+      role="list"
+      className="divide-y divide-gray-200 bg-white shadow-md rounded-md"
+    >
+      {stocks.map((stock) => (
+        <li
+          key={stock.ticker}
+          className="flex justify-between items-center gap-x-4 py-4 px-6 hover:bg-gray-50 transition-colors duration-200 cursor-pointer"
+          onClick={() => onStockClick(stock.ticker)}
+        >
+          <div className="flex items-center gap-x-4">
+            <img
+              alt={`${stock.ticker} logo`}
+              src={stock.imageUrl}
+              className="h-12 w-12 rounded-full bg-gray-100 shadow-md"
+            />
+            <div className="flex flex-col">
+              <span className="font-semibold text-lg text-gray-800">
+                {stock.ticker}
+              </span>
+              <p className="text-sm text-gray-600">{stock.price}</p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
